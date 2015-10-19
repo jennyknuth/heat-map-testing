f897250905af969e93377e39d5a4e7904eef62f1
Jenny Knuth <inkboco@gmail.com>
2015-10-19 10:17:03 -0600

    general refinements

--- TT-test-heatmap/app.js
+++ TT-test-heatmap/app.js
@@ -4,6 +4,7 @@
 //     userId: +d.userId,
 //     date: d.date,
 //     distance: +d.distance,
+//     dwDistance: +d.dwDistance
 //     type: d.type,
 //     dayPart: d.dayPart
 //   };
@@ -15,10 +16,10 @@ var margin = { top: 50, right: 0, bottom: 100, left: 30 },
     width = 960 - margin.left - margin.right,
     height = 430 - margin.top - margin.bottom,
     gridSize = Math.floor(width / 38),
-    legendElementWidth = gridSize*2,
-    colors = ["#8ff9d9","#edf8b1","#c7e9b4","#7fcdbb","#41b6c4","#1d91c0","#225ea8"], // alternatively colorbrewer.YlGnBu[9]
+    legendElementWidth = gridSize*5,
+    colors = ["#D500F9","#FFD600","#FF1744","#651FFF","#00B8D4","#64DD17","#FF6D00"], // alternatively colorbrewer.YlGnBu[9]
     days = ["M", "T", "W", "T", "F"],
-    weeks = ["A", "A", "S", "S", "S", "S", "O", "O", "O", "O", "N", "N", "N", "N", "D", "D", "D", "D", "J", "J", "J", "J", "F", "F", "F", "F", "M", "M", "M", "M", "A", "A", "A", "A", "M", "M", "M", "M"];
+    weeks = ["A", "", "S", "", "", "", "O", "", "", "", "N", "", "", "", "D", "", "", "", "J", "", "", "", "F", "", "", "", "M", "", "", "", "A", "", "", "", "M", "", "", ""];
 
 d3.tsv("TTtestData.tsv",
   function(d) {
@@ -35,12 +36,13 @@ d3.tsv("TTtestData.tsv",
   },
   function(error, data) {
     var colorScale = d3.scale.ordinal()
-        .domain(['school bus', 'rtd', 'carpool', 'walk', 'bike', 'skate/scoot', 'drive and drop'])
+        .domain(['carpool', 'school bus', 'rtd', 'drive and drop', 'walk', 'bike', 'skate/scoot'])
         .range(colors);
 
     var svg = d3.select("#chart").append("svg")
         .attr("width", width + margin.left + margin.right)
         .attr("height", height + margin.top + margin.bottom)
+        .attr("class", "svg-chart")
         .append("g")
         .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
@@ -52,7 +54,8 @@ d3.tsv("TTtestData.tsv",
           .attr("y", function (d, i) { return i * gridSize; })
           .style("text-anchor", "end")
           .attr("transform", "translate(-6," + gridSize / 1.5 + ")")
-          .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
+          .attr("class", "mono");
+          // .attr("class", function (d, i) { return ((i >= 0 && i <= 4) ? "dayLabel mono axis axis-workweek" : "dayLabel mono axis"); });
 
     var timeLabels = svg.selectAll(".weekLabel")
         .data(weeks)
@@ -62,7 +65,8 @@ d3.tsv("TTtestData.tsv",
           .attr("y", 0)
           .style("text-anchor", "middle")
           .attr("transform", "translate(" + gridSize / 2 + ", -6)")
-          .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "weekLabel mono axis axis-week" : "weekLabel mono axis"); });
+          .attr("class", "mono");
+          // .attr("class", function(d, i) { return ((i >= 7 && i <= 16) ? "weekLabel mono axis axis-week" : "weekLabel mono axis"); });
 
     var heatMap = svg.selectAll(".week")
         .data(data)
@@ -77,12 +81,13 @@ d3.tsv("TTtestData.tsv",
         .style("fill", colors[0]);
 
     heatMap.transition().duration(1000)
-        .style("fill", function(d) { return colorScale(d.type); });
+        .style("fill", function(d) { return colorScale(d.type); })
+        .style("fill-opacity", "50%");
 
     heatMap.append("title").text(function(d) { return d.type; });
 
     var legend = svg.selectAll(".legend")
-        .data([0].concat(colorScale.domain()), function(d) { return d; })
+        .data(colorScale.domain(), function(d) { return d; })
         .enter().append("g")
         .attr("class", "legend");
 
@@ -90,12 +95,17 @@ d3.tsv("TTtestData.tsv",
       .attr("x", function(d, i) { return legendElementWidth * i; })
       .attr("y", height)
       .attr("width", legendElementWidth)
-      .attr("height", gridSize / 2)
-      .style("fill", function(d, i) { return colors[i]; });
+      .attr("height", gridSize)
+      // .attr("text", function(d) {return d.type})
+      .attr("class", "bordered")
+      .attr("rx", 4)
+      .attr("ry", 4)
+      .style("fill", function(d, i) { return colors[i]; })
+      .style("fill-opacity", "50%");
 
     legend.append("text")
       .attr("class", "mono")
-      .text(function(d) { return "≥ " + Math.round(d); })
-      .attr("x", function(d, i) { return legendElementWidth * i; })
-      .attr("y", height + gridSize);
+      .text(function(d) { return d; })
+      .attr("x", function(d, i) { return (legendElementWidth * i) + 10; })
+      .attr("y", height + (gridSize/1.5 ));
 });
